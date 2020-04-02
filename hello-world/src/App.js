92d1124b607928f7acc323af23d72cf07440ce2a
EktaHeer <33094422+EktaaSharma@users.noreply.github.com>
2020-04-02 12:56:25 +0530

    First Commit

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -4,13 +4,36 @@ import './App.css';
 import Welcome from './components/functional';
 import Greet from './components/classcomp';
 import HelloJSX from './components/hello';
+import Message from './components/Message';
+import Counter from './components/Counter';
 function App() {
   return (
     <div className="App">
+{/* ------------------   Component Types   ----------------------- */}
       <h1>Types of Components</h1>
     <Welcome />
-      <Greet className/> 
-      <HelloJSX />
+    <Greet />
+    <HelloJSX /> 
+
+{/* ------------------    use of Props in functional components   ----------------------- */}
+       <Welcome name = "Brabus"  petname ="baruu">
+          <p>Our first pet.</p> 
+      </Welcome>
+      <Welcome name = "Rickson" petname ="ricks">
+        <button>Read more</button> 
+      </Welcome>
+      <Welcome name = "Scooby" petname = "sukku"/>
+      
+
+
+     {/* ------------------    use of Props in class components   ----------------------- */}
+     <Greet name = "Scooby" petname = "sukku" >
+       <p>Children property of class component</p>
+     </Greet>  
+
+     <Message /> 
+     <Counter /> 
+
     </div>
   );
 }
